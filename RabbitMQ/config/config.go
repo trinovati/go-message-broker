016f43b0b35fbf733304ce49b9f8a84f19ddc919
FabioLBoni <fabioboni96@hotmail.com>
2023-05-26 10:30:26 -0300

    environment correction

--- RabbitMQ/config/config.go
+++ RabbitMQ/config/config.go
@@ -20,9 +20,9 @@ Environmental variable used by RabbitMQ service for connection with server.
 var (
 	RABBITMQ_PROTOCOL = GetEnvOrDefault("RABBITMQ_PROTOCOL", "amqp")
 	RABBITMQ_HOST     = GetEnvOrDefault("RABBITMQ_HOST", "localhost")
-	RABBITMQ_PORT     = GetEnvOrDefault("RABBITMQ_HOST", "5672")
-	RABBITMQ_USERNAME = GetEnvOrDefault("RABBITMQ_HOST", "guest")
-	RABBITMQ_PASSWORD = GetEnvOrDefault("RABBITMQ_HOST", "guest")
+	RABBITMQ_PORT     = GetEnvOrDefault("RABBITMQ_PORT", "5672")
+	RABBITMQ_USERNAME = GetEnvOrDefault("RABBITMQ_USERNAME", "guest")
+	RABBITMQ_PASSWORD = GetEnvOrDefault("RABBITMQ_PASSWORD", "guest")
 )
 
 /*
